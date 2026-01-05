3fc2068a15ee6ae2f63047e2e2de7ac4bf0689d4
YannAlAkl <alaklyann56@gmail.com>
2026-01-05 10:20:25 -0500

    Nettoyage des conflits de fusion

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -10,11 +10,7 @@ async function db_find_user_by_username(username) {
 
     const user = rows[0];
 
-<<<<<<< HEAD
     return user;
-=======
-    return user; user:null;
->>>>>>> 5f28167a840ac769ac545c68f23ae34ab4baf599
 }
 
 async function db_find_user_by_email(email) {
@@ -25,8 +21,6 @@ async function db_find_user_by_email(email) {
     const user = rows[0];
     return user;
 }
-
-<<<<<<< HEAD
 async function db_verified_password(password,email) {
     const sql = `SELECT*FROM users where email = ? && password = ?
     `;
@@ -37,9 +31,6 @@ async function db_verified_password(password,email) {
 
     return user; 
 }
-
-=======
->>>>>>> 5f28167a840ac769ac545c68f23ae34ab4baf599
 async function db_insert_user(username, email, password, role) {
     const sql = `INSERT INTO users (username, email, password, role) VALUES (?, ?, ?, ?)`;
     const stmt = await pdo.prepare(sql);
@@ -47,10 +38,6 @@ async function db_insert_user(username, email, password, role) {
     const user_id = rows.insertId;
     return user_id;
 }
-
-<<<<<<< HEAD
-
-=======
 async function db_count_users() {
     const sql = `SELECT COUNT(*) FROM users`;
     const stmt = await pdo.prepare(sql);
